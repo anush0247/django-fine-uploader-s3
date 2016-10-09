3ba9ee8a6f230735e6a97f75733872a478b9de40
aneesh <aneesh@aneesh.cybereyelabs.io>
2016-10-09 23:42:07 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ if sys.argv[-1] == 'tag':
     os.system("git push --tags")
     sys.exit()
 
-readme = open('README.rst').read()
+readme = ''
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
