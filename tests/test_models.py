214bdc6c5723162269d019a8c2f36da19674cdba
aneesh <aneesh@aneesh.cybereyelabs.io>
2016-10-09 21:28:03 +0530

    init commit

---
+++ tests/test_models.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_django-fine-uploader-s3
+------------
+
+Tests for `django-fine-uploader-s3` models module.
+"""
+
+from django.test import TestCase
+
+from django_fine_uploader_s3 import models
+
+
+class TestDjango_fine_uploader_s3(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
